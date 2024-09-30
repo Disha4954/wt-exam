a1e55b8d5458288e12ce47dd8406eb1e15067bc0
Disha_23IT136 <152465081+Disha4954@users.noreply.github.com>
2024-09-30 12:34:42 +0530

    Add files via upload

---
+++ exam_wt/pass.js
@@ -0,0 +1,26 @@
+const generateBtn = document.getElementById('generate');
+const passwordOutput = document.getElementById('password-output');
+
+generateBtn.addEventListener('click', generatePassword);
+
+function generatePassword() {
+    const length = document.getElementById('length').value;
+    const includeUppercase = document.getElementById('uppercase').checked;
+    const includeLowercase = document.getElementById('lowercase').checked;
+    const includeNumbers = document.getElementById('numbers').checked;
+    const includeSymbols = document.getElementById('symbols').checked;
+
+    let charset = '';
+    if (includeUppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (includeLowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
+    if (includeNumbers) charset += '0123456789';
+    if (includeSymbols) charset += '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+   
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+    passwordOutput.textContent = password;
+}
